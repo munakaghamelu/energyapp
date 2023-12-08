34ebfbeeb80e4e29e06c9efaa74c733f824a07f2
munagham1 <munagham1@publicisgroupe.net>
2023-12-08 13:07:52 +0000

    frontend complete

--- energyapp/app/individuals/page.tsx
+++ energyapp/app/individuals/page.tsx
@@ -1,3 +1,15 @@
 export default function Page() {
-  return <p>Individuals Page</p>;
+  return (
+    <main className="flex min-h-screen flex-col p-6">
+      <div className="flex h-50 shrink-0 items-end rounded-lg bg-orange-500 p-4 md:h-20">
+      </div>
+      <div className="overflow-auto ...">
+        <div className="flex h-50 shrink-0 items-end rounded-lg bg-orange-500 p-4 md:h-20">
+        </div>
+
+        <div className="flex h-50 shrink-0 items-end rounded-lg bg-green-500 p-4 md:h-20">
+        </div>
+      </div>
+    </main>
+  );
 }
--- energyapp/app/profile/page.tsx
+++ energyapp/app/profile/page.tsx
@@ -4,16 +4,17 @@ export default function Page() {
       <div className="flex h-50 shrink-0 items-end rounded-lg bg-orange-500 p-4 md:h-20">
       </div>
 
-      <div className="grid justify-items-center grid-cols-4 grid-rows-6">
-        <div className="col-span-4 row-span-1 bg-orange-500">User Name</div>
-        <div className="col-span-4 row-span-3 bg-blue-500 rounded-lg">
-          <img className="h-auto max-w-lg" src={'/assets/defaultProfile.png'} alt="image of user"/>
-        </div>
-        <div className="col-span-2 row-span-2 bg-green-500">Energy Level</div>
-        <div className="col-span-2 row-span-2 bg-green-500">Prompt</div>
+      <div className="flex h-50 shrink-0 items-end rounded-lg p-4 md:h-20 text-center align-middle">
+        <h1>Username</h1>
       </div>
 
-    
+      <div className="bg-blue-500 rounded-lg">
+          <img className="h-auto max-w-lg" src={'/assets/defaultProfile.png'} alt="image of user"/>
+      </div>
+
+      <div className="bg-green-500 rounded-lg">Energy Level</div>
+      
+      <div className="bg-red-500 rounded-lg">Prompt</div>
     </main>
   );
 }
