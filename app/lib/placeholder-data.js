a047d7a303a90f29263bbb3d7c0e28fec27b6eed
munagham1 <munagham1@publicisgroupe.net>
2023-12-13 17:02:51 +0000

    adding data from db

---
+++ app/lib/placeholder-data.js
@@ -0,0 +1,34 @@
+const users = [
+  {
+    id: '410544b2-4001-4271-9855-fec4b6a6442a',
+    name: 'Spongebob',
+    email: 'spongebob@nextmail.com',
+    password: '123456',
+    role: 'user',
+  },
+  {
+    id: '3958dc9e-712f-4377-85e9-fec4b6a6442a',
+    name: 'MrKrab',
+    email: 'krab@nextmail.com',
+    password: '246810',
+    role: 'admin', 
+  },
+];
+
+const forms = [
+  {
+    user_id: user[0].id,
+    positive: 3,
+    good: 4,
+    pleasant: 3,
+    happy: 3,
+    joyful: 3,
+    contented: 5,
+    negative: 3,
+    bad: 2,
+    unpleasant: 3,
+    sad: 4,
+    afraid: 2,
+    angry: 3,
+  },
+];
