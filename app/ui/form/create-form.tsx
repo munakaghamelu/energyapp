9322cba402587c52977c4d9b2364cfa86f538ebc
munagham1 <munagham1@publicisgroupe.net>
2024-01-10 13:09:56 +0000

    completed authentication but need to fix login credentials

--- app/ui/form/create-form.tsx
+++ app/ui/form/create-form.tsx
@@ -3,6 +3,8 @@
 import { Button } from '@/app/ui/button';
 import { createForm } from '@/app/lib/actions';
 import { useFormState } from 'react-dom';
+import { PowerIcon } from '@heroicons/react/24/outline';
+import { signOut } from '@/auth';
 
 const scores = ['Positive Score', 'Good Score', 'Pleasant Score', 'Happy Score', 'Joyful Score', 'Contented Score', 'Negative Score', 'Bad Score', 'Unpleasant Score', 'Sad Score', 'Afraid Score', 'Angry Score']
 
@@ -115,6 +117,19 @@ export default function Form() {
           <div>
             <RadioButtons/>
             <Button type="submit">Submit Form</Button>
+          </div>
+          <div>
+            <form
+            action={async () => {
+              'use server';
+              await signOut();
+            }}
+            >
+              <button className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
+                <PowerIcon className="w-6" />
+                <div className="hidden md:block">Sign Out</div>
+              </button>
+            </form>
           </div>
         </div>
       </form>
